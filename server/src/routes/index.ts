4f15f1abdc9264fad95180e4a709355927bbb913
Lucas Bachega <lucas.cbachega@gmail.com>
2025-05-30 16:45:36 -0300

    verifyToken ++

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { verifyToken } from "../middlewares/verifyToken";
 import authRoutes from "./auth.routes";
 import placesRoutes from "./places.routes";
 import weatherRoutes from "./weather.routes";
 
 const router = Router();
 
-router.use("/auth", authRoutes);
-router.use("/weather", weatherRoutes);
-router.use("/places", placesRoutes);
+router.use("/auth", verifyToken, authRoutes);
+router.use("/weather", verifyToken, weatherRoutes);
+router.use("/places", verifyToken, placesRoutes);
 
 export default router;
